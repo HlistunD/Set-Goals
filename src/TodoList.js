148c1b663a6d4549427519d18769c9c34b639593
Dasha Khlistun <dashakhlistun@MacBook-Air-Dasha.local>
2023-01-23 16:29:47 -0500

    first commit

--- src/TodoList.js
+++ src/TodoList.js
@@ -15,15 +15,31 @@ export class TodoList extends Component{
     }
 
     addDeal = (input) => {
+        if (input === ""){
+            alert("Please Enter Deal")
+        }else{
         let dealArray = this.state.deal
         dealArray.push(input)
         this.setState({deal: dealArray, inputType: ""})
-    }
+    }}
 
+    dealDelete(){
+        let listArray = this.state.groceryList;
+        listArray = [];
+        this.setState({deal: listArray})
+    }
+    crossed(event){
+        const li = event.target;
+        li.classList.toggle('crossed')
+    }
+    onForm(e){
+        e.preventDefault()
+    }
 
     render() {
         return(
             <div>
+                <form onSubmit={this.onForm}>
                 <div className="container">
                 <input className="inputField" type="text" placeholder="What your future achive today?" 
                 onChange={(e) => {this.onChangeEvent(e.target.value)}} 
@@ -32,10 +48,11 @@ export class TodoList extends Component{
                 </div>
                 <ul>
                     {this.state.deal.map((element, index) => (
-                        <li key={index}>{element}</li>
+                        <li onClick={this.crossed} key={index}>{element}</li>
                     ))}
                 </ul>
-                <button className="btnDelete" onClick={() => this.dealDelete}>I GOT IT 🎉</button>
+                <button className="btnDelete" onClick={() => this.dealDelete()}>I GOT IT 🎉</button>
+            </form>
             </div>
         )
     }
